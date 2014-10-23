db0061097609b38ce0ace6b31c9e5c341482994a
Sean M. Snyder <sean.snyder@snhu.edu>
2014-10-23 02:21:20 -0400

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926034822) do
+ActiveRecord::Schema.define(version: 20141023060716) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

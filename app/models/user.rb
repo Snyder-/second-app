1c899607744e4bb4f3883575f0bfa84d2d7bb54d
Sean M. Snyder <sean.snyder@snhu.edu>
2014-10-23 19:27:22 -0400

    added password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ActiveRecord::Base
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6, maximum: 15 }    
+                    
                     
 end

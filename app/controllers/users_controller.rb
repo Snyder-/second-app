b067a7ffbb8c51190111e3bfeba10a46bec3eee8
Sean M. Snyder <sean.snyder@snhu.edu>
2014-10-25 20:46:15 -0400

    friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   # GET /users
   # GET /users.xml
@@ -60,14 +61,22 @@ class UsersController < ApplicationController
     def find_user
       @user = User.find(params[:id])
     end
+    
     def user_params
       params.require(:user).permit(:name, :email, :password,:password_confirmation)
     end
+    
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in..."
         redirect_to login_url
       end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end

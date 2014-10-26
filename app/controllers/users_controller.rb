d41dbf21e365b703eb1b6b1596b2ca711a2beb84
Sean M. Snyder <sean.snyder@snhu.edu>
2014-10-25 23:09:40 -0400

    finish user edit, index, and destroy action/tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   # GET /users
   # GET /users.xml
@@ -54,7 +55,10 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.xml
   def destroy
-  
+    @user = User.find(params[:id]).destroy
+    flash[:warning] = "User(#{@user.name}) deleted"
+    redirect_to(users_url)
+    
   end
 
   private
@@ -77,6 +81,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   setup do
     @user = users(:michael)
-    @other_user = users(:sean)
+    @other_user = users(:archer)
   end
   
   test "should get new" do
@@ -38,5 +38,19 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
   
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
 
 end
